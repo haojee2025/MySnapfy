4debb3ec40651c57f3f807f7b8a6de3e57ef7de3
haojee2025 <chjee2005@gmail.com>
2025-05-17 19:23:51 +0800

    Add url to Recycler

--- MySnapfy/MySnapfy/urls.py
+++ MySnapfy/MySnapfy/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   path('recycler/', include('recycler.urls')),
+    path('recycler/', include('recycler.urls'))
 ]
